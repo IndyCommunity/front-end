db96ac53fbce917a3b22e648cef72adee5cf62ec
Heidi Anderson <heidianderson@Heidis-MacBook-Pro.local>
2017-06-17 14:21:15 -0400

    changing web locations

--- javascript/heatMap.js
+++ javascript/heatMap.js
@@ -1,19 +1,109 @@
-const tableData = {
-  lat:39.7684,
-  long:-86.1581
-}
-const busiData = {
-  type: 1.0,
-  lat:39.7684,
-  long:-86.1581
-}
+let arr = []
+$.ajax({
+  type: "GET",
+  dataType: "json",
+  async: false,
+  url: 'http://www.indy.science/api/coordsOfCrime',
+  success: function (data) {
+    arr=data
+  }
+})
+
+
+var image='http://www.googlemapsmarkers.com/v1/275DCA/';
+/*
+let arrOffices = []
+$.ajax({
+  type: "POST",
+  dataType: "json",
+  async: false,
+  data: {
+        "zone": "Office"
+    },
+  url: 'http://www.indy.science/api/coordsByZone',
+  success: function (data) {
+    arrOffices=data
+    image ='http://www.googlemapsmarkers.com/v1/275DCA/'
+  }
+})
+let arrLI = []
+$.ajax({
+  type: "POST",
+  dataType: "json",
+  async: false,
+  data: {
+        "zone": "Light Industrial"
+    },
+  url: 'http://www.indy.science/api/coordsByZone',
+  success: function (data) {
+    arrLI=data
+    image = 'http://www.googlemapsmarkers.com/v1/CA2776/'
+  }
+})
+
+let arrHI = []
+$.ajax({
+  type: "POST",
+  dataType: "json",
+  async: false,
+  data: {
+        "zone": "Heavy Industrial"
+    },
+  url: 'http://www.indy.science/api/coordsByZone',
+  success: function (data) {
+    arrHI=data
+    image = 'http://www.googlemapsmarkers.com/v1/FBF41C/'
+  }
+})
+let arrCC = []
+$.ajax({
+  type: "POST",
+  dataType: "json",
+  async: false,
+  data: {
+        "zone": "Community Commercial"
+    },
+  url: 'http://www.indy.science/api/coordsByZone',
+  success: function (data) {
+    arrCC=data
+    image = 'http://www.googlemapsmarkers.com/v1/FB9C1C/'
+  }
+})
+let arrHC = []
+$.ajax({
+  type: "POST",
+  dataType: "json",
+  async: false,
+  data: {
+        "zone": "Heavy Commercial"
+    },
+  url: 'http://www.indy.science/api/coordsByZone',
+  success: function (data) {
+    arrHC=data
+    image = 'http://www.googlemapsmarkers.com/v1/1CFBD2/'
+  }
+})
+
+let arrPoW = []
+$.ajax({
+  type: "POST",
+  dataType: "json",
+  async: false,
+  data: {
+        "zone": "Place of Worship"
+    },
+  url: 'http://www.indy.science/api/coordsByZone',
+  success: function (data) {
+    arrPoW=data
+    image = 'http://www.googlemapsmarkers.com/v1/1CFB66/'
+  }
+})
+*/
 
 var heatmapData = [];
 
-for(let i=0;i<300;i++){
-  tableData.long+=0.0001
-  tableData.lat+=0.0001
-  heatmapData.push(new google.maps.LatLng(tableData.lat, tableData.long))
+for(let i=0;i<arr.length;i++){
+  heatmapData.push(new google.maps.LatLng(arr[i].lat, arr[i].long))
 }
 
 
@@ -21,7 +111,7 @@ var indianapolis = new google.maps.LatLng(39.7684, -86.1581);
 
 map = new google.maps.Map(document.getElementById('map-container'), {
   center: indianapolis,
-  zoom: 12,
+  zoom: 11,
   mapTypeId: 'satellite'
 });
 
@@ -29,16 +119,16 @@ var heatmap = new google.maps.visualization.HeatmapLayer({
   data: heatmapData
 });
 heatmap.setMap(map);
-var image='http://www.googlemapsmarkers.com/v1/275DCA/';
 
 
-for(let i=0;i<4;i++){
+for(let i=0;i<arrCC.length;i++){
   var marker = new google.maps.Marker({
-    position: {lat: busiData.lat, lng: busiData.long},
+    position: {lat: parseFloat(arrCC[i].lat), lng: parseFloat(arrCC[i].long)},
     map: map,
     icon: image,
     title: 'Point '+i
   });
+  /*
   if(busiData.type<6){busiData.type+=1}
   else {busiData.type=1}
   
@@ -48,8 +138,6 @@ for(let i=0;i<4;i++){
   else if(busiData.type==4){ image = 'http://www.googlemapsmarkers.com/v1/FB9C1C/'}
   else if(busiData.type==5){ image = 'http://www.googlemapsmarkers.com/v1/1CFBD2/'}
   else if(busiData.type==6){ image = 'http://www.googlemapsmarkers.com/v1/1CFB66/'}
-  
-  busiData.long+=0.01
-  busiData.lat+=0.01
+  */
 }
 
